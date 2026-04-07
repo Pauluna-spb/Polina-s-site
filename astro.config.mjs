9ac6807c3955a0881146c8234eef2dcaec05d62c
Paulina Rem <paulinarem@MacBook-Pro-Paulina.local>
2026-04-07 14:03:26 +0200

    Configure GitHub Pages deployment via Actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://pauluna-spb.github.io",
+  base: "/Polina-s-site",
   devToolbar: {
     enabled: false,
   },
