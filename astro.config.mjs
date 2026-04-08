11eaef2d898ecd6885bf9ff3998d97656396d3e3
Paulina Rem <paulinarem@MacBook-Pro-Paulina.local>
2026-04-08 10:25:28 +0200

    Fix GitHub Pages base path trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   site: "https://pauluna-spb.github.io",
-  base: "/Polina-s-site",
+  base: "/Polina-s-site/",
   devToolbar: {
     enabled: false,
   },
